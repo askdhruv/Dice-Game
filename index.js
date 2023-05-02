812a7375c2b80e379427d4ab6d5a1dc22eb213ab
askdhruv <92171175+askdhruv@users.noreply.github.com>
2023-05-02 13:31:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+//changes for first image
+var randomNumber = Math.floor(Math.random()*6) + 1;
+var firstImage = "images/dice"+randomNumber+".png";
+document.querySelector("img.img1").setAttribute("src", firstImage);
+
+//changes for second images
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+var secondImage = "images/dice"+randomNumber2+".png";
+document.querySelector("img.img2").setAttribute("src", secondImage);
+
+
+//To change the headings
+//If player 1 wins
+if(randomNumber>randomNumber2){
+    document.querySelector("h1").innerHTML = " 🚩Player 1 wins";
+}
+//If player 2 wins
+else if(randomNumber<randomNumber2){
+    document.querySelector("h1").innerHTML = " Player 2 wins🚩";
+}
+//If it draws!
+else{
+    document.querySelector("h1").innerHTML = "Draw";
+}
